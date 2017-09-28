b78479d5f9ad8299a2a5e2e753c077ee81faf5c1
Matheus Guedes <matheushg67@hotmail.com>
2017-09-28 12:28:26 -0300

    Creating and Outputting the Shopping List criado uma lista baseada no ingredient.model e feito com que ela aparecesse através do  shopping.list.component.html

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    constructor(public name: string, public amount: number){}
+    //criar o construtor assim tem o mesmo efeito que declarar as variaveis e usar this.var no construtor
+}
