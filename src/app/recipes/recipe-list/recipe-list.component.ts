54f89432316696e26f22e653f710ab3cc3f5d974
Matheus Guedes <matheushg67@hotmail.com>
2017-09-29 19:08:20 -0300

    Passing Data with Event and Property Binding (Combined) A recipe-list apenas aparece ao clicar no recipe, isso foi feito passando dados com event e property bindind

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new  EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('test recipe', 'Test desc', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg'),
-    new Recipe('test recipe', 'Test desc', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg')
+    new Recipe('Test recipe', 'Test desc', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg'),
+    new Recipe('Another test recipe', 'Test desc', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
