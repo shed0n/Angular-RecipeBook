d23e3fb7e8e72b55c0d45549ab5c9328d1c344da
Matheus Guedes <matheushg67@hotmail.com>
2017-10-03 12:57:21 -0300

    Adding Ingredients to Recipes Adicionado a lista de ingredients nas Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
@@ -6,8 +7,18 @@ export class RecipeService {
 
 
     private recipes: Recipe[] = [
-        new Recipe('Test recipe', 'Test desc', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg'),
-        new Recipe('Another test recipe', 'Test desc', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg')
+        new Recipe('Big Mac', 'A Big-Mac with double cheese', 
+        'http://d701vexhkz032.cloudfront.net/media/images/menu-content/BR/sanduiches-de-carne/bigmac_v2.png',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]),
+        new Recipe('Schnitzel', 'A super-tasty Schnitzel', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 2),
+            new Ingredient('Cheese', 4)
+        ])
       ];
 
       getRecipes() {
