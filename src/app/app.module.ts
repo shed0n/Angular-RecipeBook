5a8e9139609de1fb94993dd30cbee73a454a7d49
Matheus Guedes <matheushg67@hotmail.com>
2017-10-12 13:04:45 -0300

    Setting up Firebase and Sending PUT Requests to Save Data Adjustments made so the Firebase can be used as a dummy backend for the applicantion. The "Save Data" button was also implemented already using the http function "put".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,13 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from "./shared/dropdown.directive";
+import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from './recipes/recipe.service';
+import {DataStorageService} from './shared/data-storage.service';
 
 
 @NgModule({
@@ -40,7 +41,7 @@ import {RecipeService} from './recipes/recipe.service';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [ShoppingListService, RecipeService, DataStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
