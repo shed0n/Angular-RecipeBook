cda4c31572f155f31eb42abcd135a103140df5b0
Matheus Guedes <matheushg67@hotmail.com>
2017-10-13 10:06:41 -0300

    Signing Users In

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const appRoutes: Routes = [
     ]},
     { path: 'shopping-list', component: ShoppingListComponent},
     { path: 'signup', component: SignupComponent },
-  { path: 'signin', component: SigninComponent }
+    { path: 'signin', component: SigninComponent }
 
 ];
 
