791806bab0e86369153cc0e58f6da5b66e24b1f0
Matheus Guedes <matheushg67@hotmail.com>
2017-10-17 12:31:24 -0200

    Restructured Services to use the Child Injector

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { AuthGuard } from '../auth/auth-guard.service';
+import { AuthService } from '../auth/auth.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +22,13 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuard
   ]
 })
 export class CoreModule {
