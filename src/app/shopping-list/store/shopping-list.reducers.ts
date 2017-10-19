9accaca251386ceb06240a3b790f1c9d70e8b379
Matheus Guedes <matheushg67@hotmail.com>
2017-10-19 13:47:41 -0200

    Fixed a bug that was breaking the forms when swicthing pages (it was needed a STOP_EDIT state so the app could know we left the page and stoped editing the list)

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -45,7 +45,9 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
       ingredients[state.editedIngredientIndex] = updatedIngredient;
       return {
         ...state,
-        ingredients: ingredients
+        ingredients: ingredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
 
     case ShoppingListActions.DELETE_INGREDIENT:
@@ -53,7 +55,9 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
       oldIngredients.splice(state.editedIngredientIndex,  1);
       return {
         ...state,
-        ingredients: oldIngredients
+        ingredients: oldIngredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
     case ShoppingListActions.START_EDIT:
       const editedIngredient = {...state.ingredients[action.payload]};
@@ -61,6 +65,12 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
         ...state,
         editedIngredient: editedIngredient,
         editedIngredientIndex: action.payload
+      };
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
     default:
       return state;
