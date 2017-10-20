83428ad8e895835c9e765037a4084ac65bbb5438
Matheus Guedes <matheushg67@hotmail.com>
2017-10-20 12:40:08 -0200

    dded Auth Signin with NgRx Effects

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 
 export const TRY_SIGNUP = 'TRY_SIGNUP';
 export const SIGNUP = 'SIGNUP';
+export const TRY_SIGNIN = 'TRY_SIGNIN';
 export const SIGNIN = 'SIGNIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
@@ -11,6 +12,13 @@ export class TrySignup implements Action {
 
   constructor(public payload: {username: string, password: string}) {}
 }
+
+export class TrySignin implements Action {
+  readonly type = TRY_SIGNIN;
+
+  constructor(public payload: {username: string, password: string}) {}
+}
+
 
 export class Signup implements Action {
   readonly type = SIGNUP;
@@ -30,4 +38,4 @@ export class SetToken implements Action {
   constructor (public payload: string) {}
 }
 
-export type AuthActions = Signup | Signin | Logout | SetToken | TrySignup;
+export type AuthActions = Signup | Signin | Logout | SetToken | TrySignup | TrySignin;
